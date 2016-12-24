8123b778252aacf2170a0a34189ab5bf75975b57
Brannen Hall <Brannen@HurtadoHall.me>
2016-12-24 01:24:45 -0600

    Add a function displaySpectogram()

--- analysis.R
+++ analysis.R
@@ -7,10 +7,17 @@
 # WARNING: A Fortran compiler is needed for the proper installation of tuneR
 ###################################
 require("tuneR") 
+require("signal")
 
-
-loadWav <- function() {
+loadWave <- function() {
   file_name <- file.choose()
+# For some reason, the file.choose() function will only accept absolute locations, unix shorthand like "*" or ".." will not work
   return(readWave(file_name))
 }
+
+# This only works for signal channel wave files at the moment, also I need to work on custom windows, this displays simply raw wave data points in a 3D space
+displaySpectogram <- function(wave) {
+  return(specgram(wave@left))
+
+
 

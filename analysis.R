f3f7f6ceaa62f8b0f40b5b0df72b5aae3efb0e2e
Brannen Hall <Brannen@HurtadoHall.me>
2016-12-31 11:40:13 -0600

    Improved WAVE file input function

--- analysis.R
+++ analysis.R
@@ -11,7 +11,7 @@ if (!(require("tuneR"))){
 install.packages("tuneR")
 }
 if (!(require("signal"))){
-install.packages("signal")
+    install.packages("signal")
 }
 if (!(require("plotrix"))){
 install.packages("plotrix")
@@ -20,23 +20,35 @@ install.packages("plotrix")
 library("tuneR")
 library("signal")
 library("plotrix")
+library("tools")
 
-loadWave <- function() {
-  file_name <- file.choose()
+#loadWave <- function() {
+#  relative_file_name <- readline(prompt="Enter a file location: ")
+#  parsed_relative_file_name <- paste("\"", gsub("'","",relative_file_name), "\"", sep="")
+#   file_name <- paste(system2(paste("/bin/bash", "|", "realpath", parsed_relative_file_name), stdout=TRUE), sep="", collape="")
 # For some reason, the file.choose() function will only accept absolute locations, unix shorthand like "*" or ".." will not work
-  return(readWave(file_name))
+#  return(readWave(file_name))
+#}
+
+#Will not work with Unix shorthand such as *, but will parse relative file locations.
+loadWave <- function(unix_shorthand = FALSE) {
+  file_name <- file.choose()
+  if (unix_shorthand) {
+    file_name <- system2(paste("realpath ", "\"", gsub("\'","",file_name), "\"", sep=""), stdout=TRUE)
+  }
+  return(readWave(file_path_as_absolute(gsub("\'","",file_name))))
 }
 
 # This only works for signal channel wave files at the moment, also I need to work on custom windows, this displays simply raw wave data points in a 3D space
-displaySpectogram <- function(wave, is_right=false) {
-  if (is_right){
-    return(specgram(wave@right))
-  }
-  else {
-    return(specgram(wave@left))
-  }
+displaySpectogram <- function(wave, channel="left") {
+  return(specgram(wave@channel))
 }
 
 viewSpectum <- function(point, wave) {
   return(wave@left)
 }
+
+
+
+#returnPolarMatrix <- funciton(wave, channel="left"){
+#    for i in 0:max(wave@channel)
